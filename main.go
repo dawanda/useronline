14b97275142423754083a643dcce7c20866ccd06
Christian Parpart <trapni@gmail.com>
2016-04-07 00:07:19 +0200

    code cleanup

--- main.go
+++ main.go
@@ -11,14 +11,15 @@ import (
 	"time"
 )
 
-const (
-	cookieKey   string        = "dawanda_uo"
-	trackingTTL time.Duration = time.Second * 10
-	cookieTTL   time.Duration = time.Hour * 24 * 30
-)
+type Service struct {
+	TrackingTTL time.Duration
+	CookieTTL   time.Duration
+	CookieKey   string
+	Tracker     *Tracker
+}
 
-func httpTrack(w http.ResponseWriter, r *http.Request, tracker *Tracker) {
-	cookie, err := r.Cookie(cookieKey)
+func (service *Service) httpTrack(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(service.CookieKey)
 
 	if err != nil {
 		sid, err := createSessionID()
@@ -28,20 +29,41 @@ func httpTrack(w http.ResponseWriter, r *http.Request, tracker *Tracker) {
 			return
 		}
 		http.SetCookie(w, &http.Cookie{
-			Name:    cookieKey,
+			Name:    service.CookieKey,
 			Value:   sid,
-			Expires: time.Now().Add(cookieTTL),
+			Expires: time.Now().Add(service.CookieTTL),
 		})
-		tracker.Touch(KindNewUsers, sid)
+		service.Tracker.Touch(KindNewUsers, sid)
 	} else {
-		tracker.Touch(KindRecurring, cookie.Value)
+		service.Tracker.Touch(KindRecurring, cookie.Value)
 	}
 
-	writeEmptyGif(w, r)
+	service.writeEmptyGif(w, r)
 }
 
-func httpNotImplemented(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+func (service *Service) httpSessionsCount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, service.Tracker.GetCount(KindRecurring))
+}
+
+func (service *Service) httpNewSessionsCount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, service.Tracker.GetCount(KindNewUsers))
+}
+
+func (service *Service) httpPing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "useronline")
+}
+
+func (service *Service) httpSessionsToday(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented) // TODO
+}
+
+func (service *Service) writeEmptyGif(w http.ResponseWriter, r *http.Request) {
+	const base64GifPixel = "R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="
+	w.Header().Set("expires", time.Now().Add(service.TrackingTTL).Format(http.TimeFormat))
+	w.Header().Set("cache-control", fmt.Sprintf("max-age=%v", int(service.TrackingTTL.Seconds())))
+	w.Header().Set("content-type", "image/gif")
+	output, _ := base64.StdEncoding.DecodeString(base64GifPixel)
+	io.WriteString(w, string(output))
 }
 
 func createSessionID() (string, error) {
@@ -55,16 +77,17 @@ func createSessionID() (string, error) {
 	return uuid, nil
 }
 
-func writeEmptyGif(w http.ResponseWriter, r *http.Request) {
-	const base64GifPixel = "R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="
-	w.Header().Set("expires", time.Now().Add(trackingTTL).Format(http.TimeFormat))
-	w.Header().Set("cache-control", fmt.Sprintf("max-age=%v", int(trackingTTL.Seconds())))
-	w.Header().Set("content-type", "image/gif")
-	output, _ := base64.StdEncoding.DecodeString(base64GifPixel)
-	io.WriteString(w, string(output))
-}
-
 func main() {
+	var service = Service{
+		TrackingTTL: time.Second * 10,
+		CookieTTL:   time.Hour * 24 * 30,
+		CookieKey:   "dawanda_uo",
+	}
+
+	flag.DurationVar(&service.TrackingTTL, "tracking-ttl", service.TrackingTTL, "tracking pixel expiry timespan")
+	flag.DurationVar(&service.CookieTTL, "cookie-ttl", service.CookieTTL, "cookie expiry")
+	flag.StringVar(&service.CookieKey, "cookie", service.CookieKey, "Name of the cookie, such as browny")
+
 	httpBindAddr := flag.String("http-bind", "0.0.0.0", "HTTP service bind address")
 	httpPort := flag.Int("http-port", 8087, "HTTP service port")
 	statsdAddr := flag.String("statsd-addr", "127.0.0.1:8125", "Statsd endpoint")
@@ -76,24 +99,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create tracker. %v\n", err)
 	}
+	service.Tracker = tracker
 
-	http.HandleFunc("/uo/trck.gif", func(w http.ResponseWriter, r *http.Request) {
-		httpTrack(w, r, tracker)
-	})
-
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "useronline\n")
-	})
-
-	http.HandleFunc("/uo/newsessions/count", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, tracker.GetCount(KindNewUsers))
-	})
-
-	http.HandleFunc("/uo/sessions/count", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, tracker.GetCount(KindRecurring))
-	})
-
-	http.HandleFunc("/uo/sessions/today", httpNotImplemented)
+	http.HandleFunc("/uo/trck.gif", service.httpTrack)
+	http.HandleFunc("/ping", service.httpPing)
+	http.HandleFunc("/uo/newsessions/count", service.httpNewSessionsCount)
+	http.HandleFunc("/uo/sessions/count", service.httpSessionsCount)
+	http.HandleFunc("/uo/sessions/today", service.httpSessionsToday)
 
 	err = http.ListenAndServe(fmt.Sprintf("%v:%v", *httpBindAddr, *httpPort), nil)
 	if err != nil {
